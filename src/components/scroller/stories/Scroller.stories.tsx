75b1050d6556a87bc3643cc4c641ae34f2642205
choihooo <hochoi8621@naver.com>
2025-04-03 15:21:03 +0900

    feat: storybook 배포를 위한 빌드시 버전 오류 발생 수정

--- src/components/scroller/stories/Scroller.stories.tsx
+++ src/components/scroller/stories/Scroller.stories.tsx
@@ -1,7 +1,6 @@
-import * as React from "react";
 import { Scroller } from "../component";
-import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { within, userEvent } from "@storybook/testing-library";
+import { Meta, StoryObj } from "@storybook/react";
+import { within, userEvent } from "@storybook/test";
 import { action } from "@storybook/addon-actions";
 
 // // // //
@@ -9,28 +8,27 @@ import { action } from "@storybook/addon-actions";
 export default {
     title: "Components/Scroller",
     component: Scroller,
+} as Meta<typeof Scroller>;
+
+export const Render: StoryObj<typeof Scroller> = {
     args: {
         onClickScrollTop: action("click-scroll-top"),
         onClickScrollBottom: action("click-scroll-bottom"),
     },
-} as ComponentMeta<typeof Scroller>;
-
-const Template: ComponentStory<typeof Scroller> = (args) => (
-    <Scroller {...args} />
-);
-
-// // // //
-
-export const Render = Template.bind({});
-
-export const ScrollTop = Template.bind({});
-ScrollTop.play = async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    await userEvent.click(canvas.getByTestId("scroll-to-top"));
 };
 
-export const ScrollBottom = Template.bind({});
-ScrollBottom.play = async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    await userEvent.click(canvas.getByTestId("scroll-to-bottom"));
+export const ScrollTop: StoryObj<typeof Scroller> = {
+    ...Render,
+    play: async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+        const canvas = within(canvasElement);
+        await userEvent.click(canvas.getByTestId("scroll-to-top"));
+    },
+};
+
+export const ScrollBottom: StoryObj<typeof Scroller> = {
+    ...Render,
+    play: async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+        const canvas = within(canvasElement);
+        await userEvent.click(canvas.getByTestId("scroll-to-bottom"));
+    },
 };
