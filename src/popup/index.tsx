174fe2c53e9f75388e143d6dfbedcec24d093537
choihooo <hochoi8621@naver.com>
2025-04-05 19:23:31 +0900

    [fix] 개발자 모드에서 warning 수정

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import browser from "webextension-polyfill";
 import { Popup } from "./component";
 import "../css/app.css";
@@ -18,5 +18,11 @@ browser.tabs
         });
     })
     .then(() => {
-        ReactDOM.render(<Popup />, document.getElementById("popup"));
+        const container = document.getElementById("popup");
+        if (container) {
+            const root = createRoot(container);
+            root.render(<Popup />);
+        } else {
+            console.error("popup이라는 id를 가진 요소가 존재하지 않아요!");
+        }
     });
