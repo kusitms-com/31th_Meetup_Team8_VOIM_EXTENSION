b949c0deabea6faadfa0af973b7f56dc82b0666c
psm1st <psm1st@sookmyung.ac.kr>
2025-04-29 22:08:30 +0900

    fix : 충돌 해결

--- src/popup/component.tsx
+++ src/popup/component.tsx
@@ -7,20 +7,8 @@ export function Popup() {
         <div>
             <div className="w-[500px] y-[500px] bg-white">
                 <hr />
-<<<<<<< HEAD
-                <MenubarButton isSelected={true} text="최호" />
-                <GetDarkmode />
-                <Scroller
-                    onClickScrollTop={() => {
-                        executeScript(scrollToTopPosition);
-                    }}
-                    onClickScrollBottom={() => {
-                        executeScript(scrollToBottomPosition);
-                    }}
-                />
-=======
                 <MenubarButton isSelected={true} text="버튼" />
->>>>>>> 544db6b11bc476c1d97f21ea720cdcaefe70c9a8
+                <GetDarkmode />
             </div>
         </div>
     );
