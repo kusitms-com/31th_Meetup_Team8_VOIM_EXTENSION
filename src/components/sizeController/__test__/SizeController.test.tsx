4f225bffce17582dfb07365f71e44c11687d459e
choihooo <hochoi8621@naver.com>
2025-04-30 14:03:43 +0900

    fix: 오타 수정

--- src/components/sizeController/__test__/SizeController.test.tsx
+++ src/components/sizeController/__test__/SizeController.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { SizeController } from "../components";
+import { SizeController } from "../component";
 import renderer from "react-test-renderer";
 
 describe("SizeController", () => {
