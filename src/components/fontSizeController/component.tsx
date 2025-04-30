7d51162495d2327da00028949d3f7f51bf4bec38
choihooo <hochoi8621@naver.com>
2025-04-30 14:05:40 +0900

    refactor: fontsizeController 웹 접근성 개선

--- src/components/fontSizeController/component.tsx
+++ src/components/fontSizeController/component.tsx
@@ -14,15 +14,21 @@ export function FontSizeController() {
     return (
         <div className="w-[480px] h-[158px] flex-col flex gap-5 font-koddi text-grayscale-900 font-28-Bold">
             <div>text</div>
-            <div className="p-3 flex justify-between bg-grayscale-200 items-center rounded-[20px]">
+            <div
+                className="p-3 flex justify-between bg-grayscale-200 items-center rounded-[20px]"
+                role="group"
+                aria-labelledby="font-size-label"
+            >
                 <SizeController
                     type="minus"
                     onClick={() => handleSizeChange("minus")}
+                    alias-label="글자 크기 줄이기"
                 />
                 {textSize}
                 <SizeController
                     type="plus"
                     onClick={() => handleSizeChange("plus")}
+                    alias-label="글자 크기 늘리기"
                 />
             </div>
         </div>
