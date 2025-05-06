1d339cc092c48135d92a7e0690f1e1604df9a5da
psm1st <psm1st@sookmyung.ac.kr>
2025-05-06 13:20:05 +0900

    fix : width 정해져있는 코드 수정

--- src/iframe/iframe.tsx
+++ src/iframe/iframe.tsx
@@ -110,7 +110,7 @@ const App = () => {
                             />
                         ))}
                         <div
-                            className={`fixed right-[510px] top-[70px] w-[320px] bg-white shadow-md p-5 overflow-y-auto z-[999] transition-transform duration-300 ${
+                            className={`fixed right-[510px] top-[70px] bg-white shadow-md p-5 overflow-y-auto z-[999] transition-transform duration-300 ${
                                 isModalOpen && selectedMenu !== null
                                     ? "flex"
                                     : "hidden"
