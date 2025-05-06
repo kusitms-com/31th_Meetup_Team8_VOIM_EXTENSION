35b9c256cf02dd4d1c4a66f0cce46ac94dbc8356
psm1st <psm1st@sookmyung.ac.kr>
2025-05-06 19:01:21 +0900

    feat : 모달 글씨

--- src/components/menubar/component.tsx
+++ src/components/menubar/component.tsx
@@ -13,6 +13,7 @@ interface ModalProps {
 export function Menubar({ isOpen, onClose, children }: ModalProps) {
     const { theme } = useAppTheme();
     const isDarkMode = theme === "dark";
+
     const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
         const container = document.querySelector(
             '[data-testid="menubar-container"]',
@@ -24,9 +25,7 @@ export function Menubar({ isOpen, onClose, children }: ModalProps) {
 
     const handleResetSettings = () => {
         chrome.runtime
-            .sendMessage({
-                type: "RESET_SETTINGS",
-            })
+            .sendMessage({ type: "RESET_SETTINGS" })
             .then((response) => {
                 if (response && response.success) {
                     logger.debug("설정이 초기화되었습니다.");
@@ -37,20 +36,20 @@ export function Menubar({ isOpen, onClose, children }: ModalProps) {
             });
     };
 
-    if (!isOpen) {
-        return null;
-    }
-
     return (
         <div
-            className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-[10000] bg-black/10 backdrop-blur-xl"
+            className={`fixed top-0 left-0 w-full h-full flex items-center justify-center z-[10000] bg-black/10 backdrop-blur-xl transition-opacity duration-200 ${
+                isOpen
+                    ? "opacity-100 pointer-events-auto"
+                    : "opacity-0 pointer-events-none"
+            }`}
             onClick={handleOverlayClick}
             data-testid="menubar-overlay"
         >
             <div
-                className={` ${
+                className={`${
                     isDarkMode ? `bg-grayscale-900` : `bg-grayscale-100`
-                } fixed top-[70px] right-[20px] rounded-[30px] w-[460px] p-5 overflow-y-auto shadow-[0px_0px_4px_0px_rgba(0,0,0,0.25)] font-koddi bg-grayscale-100`}
+                } fixed top-[70px] right-[20px] rounded-[30px] w-[460px] p-5 overflow-y-auto shadow-[0px_0px_4px_0px_rgba(0,0,0,0.25)] font-koddi`}
                 data-testid="menubar-container"
             >
                 <div className="flex justify-between mb-6 font-24-Bold">
