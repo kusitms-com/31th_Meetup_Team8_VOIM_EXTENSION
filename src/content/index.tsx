08ba8d380731c050b85c2d22dd812fe0d13b3746
psm1st <psm1st@sookmyung.ac.kr>
2025-05-23 01:26:31 +0900

    feat : api 연동 진행

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -10,7 +10,9 @@ import { processImages } from "./imageHandlers/imageProcessor";
 import { MountCartSummaryApp } from "./coupang/cartSummary";
 import { checkCategoryAndRender } from "./coupang/categoryHandler";
 import { renderCouponComponent } from "./coupang/renderCouponComponent";
+import { renderInfoComponent } from "../content/coupang/renderInfoComponent";
 
+renderInfoComponent();
 checkExtensionState();
 
 document.addEventListener("DOMContentLoaded", () => {
