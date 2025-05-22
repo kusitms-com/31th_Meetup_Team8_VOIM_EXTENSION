7940da6db88778d3765c50aceeb83c25fd0cf0ca
psm1st <psm1st@sookmyung.ac.kr>
2025-05-23 01:26:22 +0900

    feat : 컴포넌트 렌더링

---
+++ src/content/coupang/renderInfoComponent.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { InfoComponent } from "../../components/productComponents/infoComponent";
+
+export const renderInfoComponent = () => {
+    console.log("[voim] Info 컴포넌트 렌더링 시작");
+    if (document.getElementById("voim-info-component")) {
+        console.log("[voim] 이미 Info 컴포넌트가 렌더링되어 있음");
+        return;
+    }
+
+    const container = document.createElement("div");
+    container.id = "voim-info-component";
+    container.style.position = "fixed";
+    container.style.bottom = "20px";
+    container.style.right = "20px";
+    container.style.zIndex = "9999";
+    document.body.appendChild(container);
+
+    const root = createRoot(container);
+    root.render(<InfoComponent />);
+
+    console.log("[voim] Info 컴포넌트 렌더링 완료");
+};
