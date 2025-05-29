cdef7660b5ed46a986b2d5ec1966bea2332340f7
choihooo <hochoi8621@naver.com>
2025-05-29 17:08:33 +0900

    feat: 리뷰 로딩 메시지 개선 및 사이드바 로딩 상태 추가

--- src/components/sidebar/component.tsx
+++ src/components/sidebar/component.tsx
@@ -154,6 +154,7 @@ export function Sidebar({
                                 keywords: [],
                             }
                         }
+                        isLoading={!reviewSummary}
                     />
                 );
             default:
